6fd970458aad4860956e935cdecf3de9bceacbec
taintpro98 <taideptrai02@gmail.com>
2024-10-16 10:51:58 +0700

    chan qua lai ngoi lam leetcode thoi

--- migrations/20241007104025_partition_follows.sql
+++ migrations/20241007104025_partition_follows.sql
@@ -3,43 +3,19 @@
 DROP TABLE IF EXISTS public.posts;
 
 CREATE TABLE public.follows (
-  id bigserial,
+  id bigserial primary key,
   follower_id uuid not null,
   followee_id uuid not null,
   "created_at" timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   "updated_at" timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  primary key (id, created_at)
-) partition by range(created_at);
-
-create index if not exists tx_follow_created_at_idx on public.follows (created_at);
+) partition by hash (follower_id);
 
 create UNIQUE index if not exists tx_u_follow_follower_followee_id_idx on public.follows (follower_id, followee_id);
 
-CREATE
-OR REPLACE FUNCTION create_follow_partitions(start_date DATE, end_date DATE) RETURN VOID AS $ $ DECLARE curr_start DATE := start_date;
-
-curr_end DATE;
-
-BEGIN WHILE curr_start < end_date LOOP curr_end := curr_start + INTERVAL '7 days';
-
-EXECUTE format(
-  'CREATE TABLE IF NOT EXISTS follow_%s PARTITION OF public."follows" FOR VALUES FROM (%L) TO (%L)',
-  to_char(curr_start, 'YYYYMMDD'),
-  curr_start,
-  curr_end
-);
-
-curr_start := curr_end;
-
-END LOOP;
-
-END;
-
-$ $ LANGUAGE plpgsql;
-
-SELECT
-  create_follow_partitions('2024-10-08' :: DATE, '2025-01-01' :: DATE);
-
+CREATE TABLE follows_part_1 PARTITION OF follows FOR VALUES WITH (MODULUS 4, REMAINDER 0);
+CREATE TABLE follows_part_2 PARTITION OF follows FOR VALUES WITH (MODULUS 4, REMAINDER 1);
+CREATE TABLE follows_part_3 PARTITION OF follows FOR VALUES WITH (MODULUS 4, REMAINDER 2);
+CREATE TABLE follows_part_4 PARTITION OF follows FOR VALUES WITH (MODULUS 4, REMAINDER 3);
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
